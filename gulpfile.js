b97e7436c5f63925c5b2d3fce13ac28065249323
Rene <rslump@guidion.net>
2017-05-09 16:05:21 +0200

    fix(*): including ts file

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('clean', require(GLOBAL.dirs.gulp + 'clean')(GLOBAL, gulp));
 gulp.task('lint', ['clean'], require(GLOBAL.dirs.gulp + 'lint')(GLOBAL, gulp));
 gulp.task('build', ['lint'], require(GLOBAL.dirs.gulp + 'build')(GLOBAL, gulp));
 gulp.task('copytemplates', ['build'], function() {
-   gulp.src('./src/**/*.pug')
+   gulp.src('./src/**/*')
    .pipe(gulp.dest(GLOBAL.dirs.dist_base));
 });
 gulp.task('test:instrument', ['copytemplates'], require(GLOBAL.dirs.gulp + 'test/instrument')(GLOBAL, gulp));
